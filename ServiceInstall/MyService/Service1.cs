42b74ca7758ded5490d02ff6975abf352a118acf
harlannorth <harlannorth@gmail.com>
2014-10-27 19:01:27 -0700

    Initial Commit
    It is working

---
+++ ServiceInstall/MyService/Service1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using log4net;
+
+namespace MyService
+{
+	public partial class Service1 : ServiceBase
+	{
+		private static readonly ILog _log = LogManager.GetLogger(typeof(Service1));
+
+		public Service1()
+		{
+			log4net.Config.XmlConfigurator.Configure(); 
+			InitializeComponent();
+		}
+
+		protected override void OnStart(string[] args)
+		{
+			_log.Info("Starting the service");
+		}
+
+		protected override void OnStop()
+		{
+			_log.Info("Stopping the service");
+		}
+	}
+}
